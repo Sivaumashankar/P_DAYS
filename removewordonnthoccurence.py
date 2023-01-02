c2f705c514500f3fd3819b40109786fa6808023c
umashankar <84461244+Sivaumashankar@users.noreply.github.com>
2023-01-02 19:58:42 +0530

    Add files via upload

---
+++ removewordonnthoccurence.py
@@ -0,0 +1,11 @@
+mylist=['geeks','for','geeks','geeks','geeks']
+word='geeks'
+n=3
+count=0
+for i in range(0,len(mylist)-1):
+    if(mylist[i]==word):
+        count =count+1
+        if(count==n):
+            del mylist[i]
+    
+print(mylist,'after updated')
