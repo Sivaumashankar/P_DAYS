7f9b645d09c64789c48629768ebcaa5a3092c885
umashankar <84461244+Sivaumashankar@users.noreply.github.com>
2023-01-13 18:08:35 +0530

    Add files via upload

---
+++ allpatterns.py
@@ -0,0 +1,23 @@
+
+#pattern program
+for i in range(0,5):
+    for k in range(0,5-1-i):
+        print(' ',end=' ')
+    for j in range(0,i+1):
+        print('*',end=' ')
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ factorialofnumber.py
@@ -0,0 +1,26 @@
+#5! = 1*2*3*4*5=120
+from math import factorial
+
+
+factorial=1
+num=5
+if num<0:
+    print("factorial does not exist for negative numbers")
+elif num==0:
+    print(" the factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        factorial=factorial*i
+    print("the factorial of",num, "is", factorial)
+
+
+
+
+
+
+
+
+
+
+
+
